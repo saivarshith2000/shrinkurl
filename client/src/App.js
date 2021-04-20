2a2d62f26024d7485440d81f5e052da359aa5d67
saivarshith2000 <hosvarshith@gmail.com>
2021-04-20 22:27:37 +0530

    pushed updated client

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,30 @@
 import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+import Home from "./pages/Home";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
+import About from "./pages/About";
 
 function App() {
-    return <div className="text-white bg-black">Hello World</div>;
+    return (
+        <Router>
+            <Switch>
+                <Route path="/signin">
+                    <SignIn />
+                </Route>
+                <Route path="/signup">
+                    <SignUp />
+                </Route>
+                <Route path="/about">
+                    <About />
+                </Route>
+                <Route path="/">
+                    <Home />
+                </Route>
+            </Switch>
+        </Router>
+    );
 }
 
 export default App;
---
+++ client/src/components/NavBar.js
@@ -0,0 +1,37 @@
+import { Link } from "react-router-dom";
+
+function NavBar() {
+    // Render the navigation urls
+    const renderNav = () => {
+        let urls = [
+            { text: "Sign In", url: "/signin" },
+            { text: "Sign Up", url: "/signup" },
+            { text: "About", url: "/about" },
+        ];
+        return urls.map((url) => (
+            <div
+                className="p-2 m-auto mx-4 rounded-sm hover:bg-blue-600 hover:text-gray-50"
+                key={url.url}
+            >
+                <Link className="text-xl" to={url.url}>
+                    {url.text}
+                </Link>
+            </div>
+        ));
+    };
+
+    return (
+        <div className="fixed z-50 w-full p-4 shadow-lg bg-gray-50">
+            <div className="flex flex-row justify-between w-2/3 m-auto align-middle">
+                <div className="p-2 my-auto text-4xl font-bold align-middle bg-blue-600 rounded-sm text-gray-50">
+                    <Link to="/">ShrinkUrl</Link>
+                </div>
+                <div className="flex flex-row justify-between align-middle">
+                    {renderNav()}
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default NavBar;
